5397e2a2de6de700f10341b822640bf851b2e452
Nguyen Khac Trung <100515189+Khactrung1912@users.noreply.github.com>
2023-12-24 22:01:59 +0700

    Update template.cpp
    Sửa hai hàm _max,_min

--- template.cpp
+++ template.cpp
@@ -13,8 +13,8 @@ using namespace std;
 const int mod=int(1e9+7);
 template<class A,class B> inline void add(A &a,B b) { a+=b;while (a>=mod) a-=mod;}
 template<class A,class B> inline void sub(A &a,B b) { a-=b;while (a>=mod) a-=mod;while (a<0) a+=mod;}
-template<class A,class B> bool _max(A a,B b) {if (a<b) return a=b,1; return 0;}
-template<class A,class B> bool _min(A a,B b) {if (a>b) return a=b,1; return 0;}
+template<class A,class B> bool _max(A &a,B b) {if (a<b) return a=b,1; return 0;}
+template<class A,class B> bool _min(A &a,B b) {if (a>b) return a=b,1; return 0;}
 int main()
 {
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
