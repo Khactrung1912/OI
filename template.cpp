b2b4e1f4cb9adc3939ca3fb6c033297af9c7f5da
Nguyen Khac Trung <100515189+Khactrung1912@users.noreply.github.com>
2024-01-09 11:51:06 +0700

    Thêm danh sách hướng đi trên bảng

--- template.cpp
+++ template.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+ #include <bits/stdc++.h>
 using namespace std;
 #define fi first
 #define se second
@@ -12,6 +12,8 @@ using namespace std;
 #define ull unsigned long long
 #define name "file"
 const int mod=int(1e9+7);
+int pi[8]={0,0,1,-1,1,1,-1,-1};
+int pj[8]={-1,1,0,0,1,-1,1,-1};
 template<class A,class B> inline void add(A &a,B b) { a+=b;while (a>=mod) a-=mod;}
 template<class A,class B> inline void sub(A &a,B b) { a-=b;while (a>=mod) a-=mod;while (a<0) a+=mod;}
 template<class A,class B> bool _max(A &a,B b) {if (a<b) return a=b,1; return 0;}
@@ -19,7 +21,7 @@ template<class A,class B> bool _min(A &a,B b) {if (a>b) return a=b,1; return 0;}
 int main()
 {
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	if (fopen(name".inp","r",stdin)) 
+	if (fopen(name".inp","r")) 
 		{
 			freopen(name".inp","r",stdin);
 			freopen(name".out","w+",stdout);
