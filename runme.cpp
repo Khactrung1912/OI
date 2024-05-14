64ed97c0361a050acdf35a4892fad359c3166350
Nguyen Khac Trung <100515189+Khactrung1912@users.noreply.github.com>
2024-05-14 22:58:23 +0700

    Update runme.cpp
    Sửa c++ version mặc định

--- runme.cpp
+++ runme.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string const BRUTE = "BRUTE";//file code trâu
 string const SOL = "SOL";//file code cần kiểm tra
 string const NAME = "A"; //file input,output và ans
-string const C_VERSION = "17"; //flag version c++
+string const C_VERSION = "14"; //flag version c++
 int const N = 100; //số lượng test cần chạy.
 
 void compile(string file){
@@ -14,7 +14,7 @@ void compile(string file){
 }
 
 void run(string file){
-	const string com = "./" + file;
+	const string com = ".\\" + file;
 	system(com.c_str()); 
 }
 
